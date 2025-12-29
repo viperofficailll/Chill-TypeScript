4fd56b553ad92088bcb41f63998a1875c10e1e9c
viperofficailll <sharadhapokharel777@gmal.com>
2025-12-29 20:51:01 +0545

    restart

--- e-strore/Backend/src/app.ts
+++
@@ -1,5 +0,0 @@
-import  express from "express";
-import router from "./routes/Auth.routes";
-export const app =express()
-
-app.use('/api/v1/auth',router)
--- e-strore/Backend/src/routes/Admin.routes.ts
+++
@@ -1,4 +0,0 @@
-import express from 'express'
-export const userrouter = express.Router()
-
-userrouter.use()
